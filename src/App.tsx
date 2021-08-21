5e994a2883eeccd3288244e1689fa553f6299267
ZHANGKE <kevin.zhang@mpac.ca>
2021-08-20 22:25:09 -0400

    fix eslint issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import './App.css';
-import axios from 'axios';
 
 const App = () => {
-  axios.defaults.headers.common['Authorization'] = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
+  axios.defaults.headers.common.Authorization = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
 
   return (
     <SearchBar />
   );
-}
+};
 
 export default App;
